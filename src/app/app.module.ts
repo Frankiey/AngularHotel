a8c98458149ef9b269b68754df0d7b7b02eb9df3
frankieboym <franknoorloos@hotmail.com>
2017-11-21 09:48:23 +0100

    added basic http request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { WorkersOverviewComponent } from './workers-overview/workers-overview.component';
 import { AddWorkerComponent } from './add-worker/add-worker.component';
 import { AppRoutingModule } from './app-routing.module';
 import { WorkerService } from './worker.service';
-import { EditWorkerComponent } from './edit-worker/edit-worker.component'
+import { EditWorkerComponent } from './edit-worker/edit-worker.component';
 
 
 @NgModule({
@@ -16,13 +16,13 @@ import { EditWorkerComponent } from './edit-worker/edit-worker.component'
     AppComponent,
     WorkersOverviewComponent,
     AddWorkerComponent,
-    EditWorkerComponent//,
-    // AppRoutingModule
+    EditWorkerComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [WorkerService],
   bootstrap: [AppComponent]
--- src/app/worker.service.ts
+++ src/app/worker.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Worker} from './worker';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { catchError, map, tap } from 'rxjs/operators';
 
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Injectable()
 export class WorkerService {
+  private reqResUrl = 'https://reqres.in/api/';
 
   workers: Worker[];
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.workers = [
       {
         id: 1,
@@ -30,8 +38,30 @@ export class WorkerService {
     ];
    }
 
-getWorker(id: number): Worker{
+getWorker(id: number): Worker {
   return this.workers.find( x => x.id === id);
 }
+
+getWorkers(): Observable<Worker[]> {
+
+  return this.http.get<Worker[]>(this.reqResUrl + 'users')
+  .pipe(
+    tap(_ => console.log(_)),
+    catchError(this.handleError('getWorkers', []))
+  );
+}
+
+private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    console.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
 
 }
