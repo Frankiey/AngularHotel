922ccc5b1d49839985ca1df506947e51dae6a7ca
frankieboym <franknoorloos@hotmail.com>
2017-11-21 15:13:50 +0100

    added update and delete with http api

---
+++ src/app/Api-types.ts
@@ -0,0 +1,18 @@
+export interface UserList {
+  page: number;
+  per_page: number;
+  total: number;
+  total_pages: number;
+  data: User[];
+}
+
+export interface SingleUser {
+  data: User;
+}
+
+export interface User {
+  id: number;
+  first_name: string;
+  last_name: string;
+  avatar: string;
+}
