324317b910a637d1e65f36096d8fd1bc946f09cb
frankieboym <franknoorloos@hotmail.com>
2017-11-22 14:02:27 +0100

    added avatar to edit

--- src/app/edit-worker/edit-worker.component.ts
+++ src/app/edit-worker/edit-worker.component.ts
@@ -18,6 +18,7 @@ export class EditWorkerComponent implements OnInit {
   inputLastName: string;
   inputRole: string;
   inputStartDate: Date;
+  inputAvatar: string;
 
   deleted = false;
   index: number;
@@ -50,6 +51,7 @@ export class EditWorkerComponent implements OnInit {
     this.inputLastName = user.last_name;
     this.inputRole = 'Schoonmaker';
     this.inputStartDate = undefined;
+    this.inputAvatar = user.avatar;
 
     console.log('fields filled');
   }
--- src/app/workers-overview/workers-overview.component.ts
+++ src/app/workers-overview/workers-overview.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Worker } from '../worker';
 import { WorkerService } from '../worker.service';
 import { UserList, User } from '../Api-types';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-workers-overview',
@@ -15,7 +16,7 @@ export class WorkersOverviewComponent implements OnInit {
 
   workers: Worker[];
 
-  constructor(public workerService: WorkerService) {
+  constructor(public workerService: WorkerService, private router: Router) {
     // this.workerService.getWorkers();
     // this.workersDisplay = this.workerService.workers;
    }
@@ -54,7 +55,7 @@ export class WorkersOverviewComponent implements OnInit {
     this.workers = this.workers.filter(worker => worker.first_name.includes(this.inputName));
     console.log('done filtering');
   }
-  
+
 
   pageClick(page: number): void {
     this.currentPage = page;
@@ -65,5 +66,10 @@ export class WorkersOverviewComponent implements OnInit {
 
       this.workers = workers2;
     });
+  }
+
+  redirectTo(id: number) {
+    this.router.navigateByUrl('editworker/' + id);
+    console.log(id);
   }
 }
