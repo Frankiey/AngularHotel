269c873bbca2482c99c6fc46ad8288dcfe18c148
trozie <twanrozie@gmail.com>
2017-11-23 15:16:19 +0100

    added e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -11,4 +11,41 @@ describe('angular-fancy-hotel App', () => {
     page.navigateTo();
     expect(page.getParagraphText()).toEqual('Welkom op het werknemers portaal van hotel fancypants');
   });
+
+  it('should display welcome message', () => {
+    page.navigateToAddWorker();
+    expect(page.getAddWorkerTitle()).toEqual('Werknemer Toevoegen');
+  });
+
+  it('should display "Gelukt!" message after pressing toevoegen', () => {
+    page.navigateToAddWorker();
+    page.getToevoegenButton().click();
+    expect(page.getSuccess().getText()).toContain('Gelukt!');
+  });
+
+  it('checks if dropdown contains Schoonmaker', () => {
+    page.navigateToAddWorker();
+    expect(page.getSchoonmaker().getText()).toEqual('Schoonmaker');
+  });
+
+  it('checks if dropdown contains all roles', () => {
+    page.navigateToAddWorker();
+    expect(page.getDropdownLength().count()).toEqual(8);
+  });
+
+  it('checks if the add button redirects to the addworker page', () => {
+    page.navigateToOverview();
+    page.getAddButton().click();
+    expect(page.getAddWorkerTitle()).toEqual('Werknemer Toevoegen');
+  });
+
+  it('checks if table contains 3 rows', () => {
+    page.navigateToOverview();
+    expect(page.getNumberofRows().count()).toEqual(3);
+  });
+
+  it('checks if table contains id 1 as first id', () => {
+    page.navigateToOverview();
+    expect(page.getFirstIdofRow().getText()).toEqual('1');
+  });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,54 @@ export class AppPage {
     return browser.get('/');
   }
 
+  navigateToAddWorker() {
+    return browser.get('/addworker');
+  }
+
+  navigateToOverview() {
+    return browser.get('/overview');
+  }
+
+// ===================== navigator / selector devider ====================================== //
+
+
+// === frontpage test === //
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
+  }
+
+// === add worker test === //
+  getToevoegenButton() {
+    return element(by.css('#addWorker'));
+  }
+
+  getSuccess() {
+    return element(by.css('#addSuccess'));
+  }
+
+  getSchoonmaker() {
+    return element(by.cssContainingText('option', 'Schoonmaker'));
+  }
+
+  getDropdownLength() {
+    return element.all(by.css('select[name="role"] option'));
+  }
+
+  getAddWorkerTitle() {
+    return element(by.css('h2')).getText();
+  }
+
+// === overview worker tests === //
+
+  getAddButton() {
+  return element(by.css('a[href="/addworker"]'));
+  }
+
+  getNumberofRows() {
+    return element.all(by.css('tbody tr'));
+  }
+
+  getFirstIdofRow() {
+    return element(by.css('tbody tr:first-child td:first-child'));
   }
 }
